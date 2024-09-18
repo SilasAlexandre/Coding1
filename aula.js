24a18ef8c8f6ddd9ccc0e3f8e27b8192a74dc92c
SilasAlexandre <150928718+SilasAlexandre@users.noreply.github.com>
2024-09-18 13:55:53 +0000

    criação de prompt

---
+++ aula.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')();
+
+nome = prompt("Qual o seu nome?")
+
+let nome  = prompt("Qual é o seu nome?")
+
+console.log(`Olá, ${nome}! Boa Vindas a Faculdade Senac`)
+
+
